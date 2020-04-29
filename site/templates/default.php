c5f3b6a6e53f70e543e30172ceea2fcc89dbb065
Pam Willenz <pam@positek.net>
2020-04-29 17:40:29 -0400

    add css, pages, content

--- site/templates/default.php
+++ site/templates/default.php
@@ -3,6 +3,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $site->title() ?></title>
+
+  <?= css('assets/css/index.css') ?>
+
+
 </head>
 <body>
 
@@ -11,12 +15,13 @@
   </header>
 
   <nav class="menu">
-    <?php foreach ($site->children() as $subpage): ?>
+    <?php foreach ($site->children()->listed() as $subpage): ?>
     <a href="<?= $subpage->url() ?>"><?= $subpage->title() ?></a>
     <?php endforeach ?>
   </nav>
 
   <h1><?= $page->title() ?></h1>
+  <br><br>
   <h2><?= $page->heading() ?></h3>
   <h3><?= $page->text() ?></h3>
   <h3><?= $page->footer() ?></h3>
