29c91a4f7d2031cc7d27ce76dde604eceba68576
Pam Willenz <pam@positek.net>
2020-04-30 14:48:04 -0400

    build out home, snippets, structure

---
+++ site/snippets/header.php
@@ -0,0 +1,24 @@
+<html lang="eng">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= $site->title() ?></title>
+
+
+</head>
+<body>
+
+  <header>
+    <a class="branding" href="<?= $site->url() ?>"><?= $site->title() ?></a>
+
+    <?= css('assets/css/index.css') ?>
+
+  </header>
+
+  <nav class="menu">
+    <?php foreach ($site->children()->listed() as $subpage): ?>
+    <a href="<?= $subpage->url() ?>"><?= $subpage->title() ?></a>
+    <?php endforeach ?>
+  </nav>
+
+  
---
+++ site/templates/home.php
@@ -0,0 +1,13 @@
+<?php snippet('header') ?>
+ 
+ 
+  <?= css('assets/css/index.css') ?>
+    <style type="text/css">
+      body {background-image: url("assets/images/cda-interview-guide.jpg");
+        background-repeat: no-repeat;
+        background-size: 100%;
+      }
+    </style>
+
+
+<?php snippet('footer') ?>
